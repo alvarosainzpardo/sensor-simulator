02d31195947e0c7efd168c0fb3511105f0c21b96
alvarosainzpardo <alvarosainzpardo@gmail.com>
2016-05-03 11:52:55 +0200

    gps v1 and dgt demo

--- src/sensor-simulator.py
+++ src/sensor-simulator.py
@@ -3,9 +3,11 @@
 # Copyright 2016 Telefonica Soluciones, S.A.U
 # Developed by Alvaro Sainz-Pardo (@asainzp), Mar 2016.
 
+from __future__ import division
 import ConfigParser
 from datetime import datetime
-from __future__ import division
+import json
+from math import cos, sin, asin, sqrt, radians
 import random
 import requests
 import sys
@@ -44,6 +46,12 @@ class Sensor:
 				measure['value'] = float('{0:.2f}'.format(random.uniform(measure['from'], measure['to'])))
 			elif measure['method'] in ['randval', 'randomval', 'random_val', 'random_value']:
 				measure['value'] = random.choice(measure['values'])
+			elif measure['method'] in ['listval', 'list_val']:
+				index = measure['index']
+				if index < len(measure['values']) - 1:
+					index += 1
+				measure['index'] = index
+				measure['value'] = measure['values'][index]
 			elif measure['method'] in ['incrandint', 'increment_randint', 'increment_random_int']:
 				measure['value'] += random.randint(measure['from'], measure['to'])
 			elif measure['method'] in ['incrandfloat', 'increment_randfloat', 'increment_random_float']:
@@ -113,8 +121,90 @@ class Sensor:
 
 			# Special method for specific types of sensors
 
-			# Moving sensor with GPS coordinates
-			elif measure[method'] in ['gps_line', 'gps_pol', 'gps_polygon']:
+			# Moving sensor with GPS coordinates along a line of GPS points
+			elif measure['method'] in ['gps_line']:
+				index = measure['index']
+				# We have not reached the end of the list of gps points
+				if index < len(measure['gpspoints']) - 1:
+					lat1 = float(measure['value'].split(',')[0].strip())
+					lon1 = float(measure['value'].split(',')[1].strip())
+					lat2 = float(measure['gpspoints'][index+1].split(',')[0].strip())
+					lon2 = float(measure['gpspoints'][index+1].split(',')[1].strip())
+					# radius of the Earth in meters
+					R = 6371000
+					# convert decimal degrees to radians
+					rlat1, rlon1, rlat2, rlon2 = map(radians, [lat1, lon1, lat2, lon2])
+    			# haversine formula
+					dlon = rlon2 - rlon1
+					dlat = rlat2 - rlat1
+					a = sin(dlat/2)**2 + cos(lat1) * cos(lat2) * sin(dlon/2)**2
+					c = 2 * asin(sqrt(a))
+					# distance between points using haversine formula
+					distance_to_next_point = R * c
+					# distance run with _velocity in timeout time
+					distance_run = ((_velocity * 1000) / 3600) * self.timeout
+					# if the moving thing with _velocity moves farther than next point
+					# in timeout time
+					if distance_run >= distance_to_next_point:
+						# move the thin directly to the next point
+						# and advance the index
+						index += 1
+						measure['index'] = index
+						measure['value'] = measure['gpspoints'][index]
+					else:
+						dlat = lat2 - lat1
+						dlon = lon2 - lon1
+						ratio_run = distance_run / distance_to_next_point
+						inclat = dlat * ratio_run
+						inclon = dlon * ratio_run
+						measure['value'] = '{0:.6f},{1:.6f}'.format(lat1+inclat, lon1+inclon)
+
+			# Moving sensor with GPS coordinates along a polygon of GPS points
+			elif measure['method'] in ['gps_pol', 'gps_polygon']:
+				index = measure['index']
+				lat1 = float(measure['value'].split(',')[0].strip())
+				lon1 = float(measure['value'].split(',')[1].strip())
+				# We have not reached the end of the list of gps points
+				if index < len(measure['gpspoints']) - 1:
+					lat2 = float(measure['gpspoints'][index+1].split(',')[0].strip())
+					lon2 = float(measure['gpspoints'][index+1].split(',')[1].strip())
+				# End of list: next element is first
+				else:
+					lat2 = float(measure['gpspoints'][0].split(',')[0].strip())
+					lon2 = float(measure['gpspoints'][0].split(',')[1].strip())
+				# radius of the Earth in meters
+				R = 6371000
+				# convert decimal degrees to radians
+				rlat1, rlon1, rlat2, rlon2 = map(radians, [lat1, lon1, lat2, lon2])
+    		# haversine formula
+				dlon = rlon2 - rlon1
+				dlat = rlat2 - rlat1
+				a = sin(dlat/2)**2 + cos(lat1) * cos(lat2) * sin(dlon/2)**2
+				c = 2 * asin(sqrt(a))
+				# distance between points using haversine formula
+				distance_to_next_point = R * c
+				# distance run with _velocity in timeout time
+				distance_run = ((_velocity * 1000) / 3600) * self.timeout
+				# if the moving thing with _velocity moves farther than next point
+				# in timeout time
+				if distance_run >= distance_to_next_point:
+					# move the thin directly to the next point
+					# and advance the index
+					# taking into account if we are or not at the end of the list
+					if index < len(measure['gpspoints']) - 1:
+						index += 1
+					else:
+						index = 0
+					measure['index'] = index
+					measure['value'] = measure['gpspoints'][index]
+				else:
+					dlat = lat2 - lat1
+					dlon = lon2 - lon1
+					ratio_run = distance_run / distance_to_next_point
+					inclat = dlat * ratio_run
+					inclon = dlon * ratio_run
+					measure['value'] = '{0:.6f},{1:.6f}'.format(lat1+inclat, lon1+inclon)
+#				print 'coche: ', self.id, ' index: ', measure['index'],' coords: ', measure['value']
 
 			# Streetline parking sensor
 			elif measure['method'] in ['streetline_state', 'parking_state']:
@@ -125,6 +215,10 @@ class Sensor:
 					_streetline_availability = 1
 			elif measure['method'] in ['streetline_availability', 'parking_availabiliy']:
 				measure['value'] = _streetline_availability
+
+			# We store the velocity of moving objetcs
+			if measure['name'] in ['vel', 'velocity', 'velocidad']:
+				_velocity = measure['value']
 
 	def _send_measures(self):
 		url = 'http://'+self.host+':'+self.port+'/iot/d?k='+self.apikey+'&i='+self.id
@@ -137,6 +231,8 @@ class Sensor:
 				first_measure = False
 			payload += measure['name'] + '|' + str(measure['value'])
 		r = requests.post(url, data=payload, headers='')
+#		print url
+#		print payload
 #		print str(r.status_code) + r.text
 
 	def _run(self):
@@ -157,6 +253,8 @@ class Sensor:
 		self.timeout = eval(get_sensor_option(config, self.id, 'timeout'))
 		self.measures = eval(get_sensor_option(config, self.id, 'measures'))
 		for measure in self.measures:
+			if measure['method'] in ['listval', 'list_val']:
+				measure['index'] = -1
 			if measure['method'] in ['timeseqfloat', 'timeseq_float', 'time_seq_float',
 									'timeseqint', 'timeseq_int', 'time_seq_int',
 									'timeseqvalue', 'timeseq_value', 'time_seq_value']:
@@ -170,7 +268,9 @@ class Sensor:
 				for datapoint in measure['datapoints']:
 					datapoint['time'] = datetime.strptime(datapoint['time'], '%H:%M:%S').time()
 			if measure['method'] in ['gps_line', 'gps_pol', 'gps_polygon']:
-
+				measure['gpspoints'] = json.loads(config.get('data', measure['gpspoints']))
+				measure['index'] = 0
+				measure['value'] = measure['gpspoints'][0]
 		self.runtime = 0
 		self._ready = True
 
